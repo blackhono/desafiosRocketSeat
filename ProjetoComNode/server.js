226a90d0a0c15a9b0b5233db9699a132fd7187c9
Diego Lepamar dos Santos <diegolar25@outlook.com>
2020-04-26 20:55:32 -0300

    Implementação de Docker ao projeto

--- ProjetoComNode/server.js
+++ ProjetoComNode/server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect('mongodb://192.168.99.100:27017/nodeapi',{
+     useUnifiedTopology: true,
+     useNewUrlParser:true,
+});
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+//Primeira Rota
 app.get('/',(req,res) => {
-    res.send('eita caraiba');
+    Product.create({ title: 'React Native',
+description:'Build native apps with react',
+url:'http:github.com/facebook/react-native'});
+    
+    return res.send('Hello Rocketseat');
 })
 
 
