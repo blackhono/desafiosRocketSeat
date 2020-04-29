8f317841085405b3cd0c59a0c145697962d708c1
Diego Lepamar dos Santos <diegolar25@outlook.com>
2020-04-29 18:16:09 -0300

    Adição de controller e routes

--- ProjetoComNode/server.js
+++ ProjetoComNode/server.js
@@ -1,27 +1,31 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir')
 
 //Iniciando o App
 const app = express();
+//usado para dar permissão para envio
+//de informações json para a aplicação.
+app.use(express.json());
+/// para acessar a aplicação  se usa o cors 
+/// da para limitar quem teria acesso passando como parametro
+app.use(cors());
 
 //Iniciando o DB
-mongoose.connect('mongodb://192.168.99.100:27017/nodeapi',{
+mongoose.connect(
+    'mongodb://192.168.99.100:27017/nodeapi',
+    {
      useUnifiedTopology: true,
      useNewUrlParser:true,
-});
+    }
+);
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
 
 //Primeira Rota
-app.get('/',(req,res) => {
-    Product.create({ title: 'React Native',
-description:'Build native apps with react',
-url:'http:github.com/facebook/react-native'});
-    
-    return res.send('Hello Rocketseat');
-})
+app.use('/api', require("./src/routes"));
+
 
 
 app.listen(3001);
